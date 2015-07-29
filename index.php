50701bd8e8d91b9e382509205efcc54dbf30ad5c
Kenneth-Anthony <kchavez.bills@gmail.com>
2015-07-28 21:15:11 -0600

    Deleted the git hub repository. Started a new repository. Added bootstrap

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYP html>
+<html lang="eng">
+	<head>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
+
+		<!-- Bootstrap Latest compiled and minified CSS -->
+		<link type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet"/>
+
+		<!-- Optional Bootstrap theme -->
+		<link type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css" rel="stylesheet"/>
+
+		<!-- kenneth anthony custom style sheet-->
+		<link rel="stylesheet" type="text/css" href="kennethanthony.css">
+		///////////////////////////////////////////////////// -->
+
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script type="text/javascript" src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script type="text/javascript" src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+		<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
+		<script type="text/javascript" src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.13.1/jquery.validate.min.js"></script>
+		<script type="text/javascript" src="//ajax.aspnetcdn.com/ajax/jquery.validate/1.12.1/additional-methods.min.js"></script>
+
+		<!-- Latest compiled and minified Bootstrap JavaScript, all compiled plugins included -->
+		<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+		<title>Bootstrap 101 Template (DDC Version!)</title>
+	</head>
+	<header>
+		<div class="header logo">
+
+		</div>
+	</header>
+	<body>
+		<p>
+Kenneth Anthony
+
+Purpose
+Hello, my name is Kenneth Anthony Chavez the specific purpose of this website is show case my experience as a graphic
+			designer. This will feature select works through out my career which includes branding, design, marketing,
+			illustration, photography, motion graphics, website and software engineering.?? Purpose The demographic for the my website is businesses looking to advance their presences on the web. The services may include branding, graphic design, public realations, illustration, photography, multimedia services, and social media marketing.
+Goal
+The website design will be clean, modern, minimalist and visually appealing. This is accomplished through a scrolling bar highlighting each portfolio category (essentially?becoming menu structure). The website will be image based.
+
+		</p>
+	</body>
+</html>
+
