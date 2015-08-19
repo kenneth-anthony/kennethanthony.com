eed240627724b064f3cb4cc5850688ee62c433bc
Kenneth-Anthony <kchavez.bills@gmail.com>
2015-08-18 23:22:06 -0600

    finished the logos added them to git

--- php/lib/footer.php
+++ php/lib/footer.php
@@ -1,1 +1,4 @@
-<?php
+<div class="container" xmlns="http://www.w3.org/1999/html">
+	<div class="col-md-3">
+		<a class "home-link" href= ".">
+		<img src=\media\logos\Kenneth Anthony Logo\KA-Logo SVG\Kenneth-Anthony-Logo-horizontal-dkbg.svgt" alt=logo/>
--- php/lib/header.php
+++ php/lib/header.php
@@ -1,7 +1,11 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Kenneth Anthony
- * Date: 8/15/2015
- * Time: 11:30 PM
- */
+<div class="container" xmlns="http://www.w3.org/1999/xhtml">
+	<div class="row">
+		<a class="home-link" href="."
+			<div class="col-md-3"
+				  <span=class="title">
+					  <img class="logo" src="media/KA-Logo SVG/Kenneth-Anthony-Logo-horizontal-dkbg.svg" alt="Kenneth Anthony"/>
+					  </a>
+			<div class="col-md-5">
+				// Nav Bar goes here
+
+	</div>
