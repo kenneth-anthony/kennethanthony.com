77c4f2a4568b9d871efbd4edfa24ff7b3a9c34a4
Kenneth-Anthony <kchavez.bills@gmail.com>
2015-08-18 23:34:23 -0600

    finished the logos added them to git

--- php/lib/header.php
+++ php/lib/header.php
@@ -1,11 +1,24 @@
-<div class="container" xmlns="http://www.w3.org/1999/xhtml">
-	<div class="row">
-		<a class="home-link" href="."
-			<div class="col-md-3"
-				  <span=class="title">
-					  <img class="logo" src="media/KA-Logo SVG/Kenneth-Anthony-Logo-horizontal-dkbg.svg" alt="Kenneth Anthony"/>
-					  </a>
-			<div class="col-md-5">
-				// Nav Bar goes here
+<?php
+require_once("lib/header.php");
+?>
+<html>
+	<body>
+		<header>
 
-	</div>
+
+
+		<div class="container" xmlns="http://www.w3.org/1999/xhtml">
+			<div class="row">
+				<a class="home-link" href="."
+					<div class="col-md-3"
+						  <span=class="title">
+							  <img class="logo" src="media/KA-Logo SVG/Kenneth-Anthony-Logo-horizontal-dkbg.svg" alt="Kenneth Anthony"/>
+							  </a>
+					<div class="col-md-5">
+						// Nav Bar goes here
+					</div>
+				</div>
+			<div
+		</header>
+	</body>
+</html>
