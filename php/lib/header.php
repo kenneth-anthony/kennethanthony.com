b80850ec35d5fa03508659589b232de3930aa497
Kenneth Chavez <kchavez.bills@gmail.com>
2015-08-30 17:07:44 -0600

    pushing from the mac

--- php/lib/header.php
+++ php/lib/header.php
@@ -1,24 +1,17 @@
-<?php
-require_once("lib/header.php");
-?>
-<html>
-	<body>
-		<header>
+<div class="container">
+	<div class="row">
+		<div class="col-md-4">
+			<span class="logo">
+				<img src="/media/logos/Kenneth%20Anthony%20Logo/KA-Logo%20SVG/Kenneth-Anthony-Logo-horizontal-dkbg.svg" alt="Kenneth Anthony Logo"/>
+			</span>
+		</div>
+		<div class="col-md-6">
+			<nav class="navbar navbar-default navbar-fixed-top">
+				<div class="navbar-container">
 
-
-
-		<div class="container" xmlns="http://www.w3.org/1999/xhtml">
-			<div class="row">
-				<a class="home-link" href="."
-					<div class="col-md-3"
-						  <span=class="title">
-							  <img class="logo" src="media/KA-Logo SVG/Kenneth-Anthony-Logo-horizontal-dkbg.svg" alt="Kenneth Anthony"/>
-							  </a>
-					<div class="col-md-5">
-						// Nav Bar goes here
-					</div>
 				</div>
-			<div
-		</header>
-	</body>
-</html>
+			</nav>
+		</div>
+
+	</div>
+</div>
